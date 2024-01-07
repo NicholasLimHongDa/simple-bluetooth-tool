67b7a3c19bad5025ea338fff54348528c97c6192
Nicholas Lim Hong Da <nicholas.lim.hong.da.cs@gmail.com>
2024-01-07 14:39:12 +0800

    Added a function to request pairing to a Bluetooth device (this is not yet tested)

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -31,6 +31,36 @@ namespace WindowsFormsApp1
             // BluetoothWin32Authentication authenticator = new BluetoothWin32Authentication(Win32AuthCallbackHandler);
         }
 
+        private bool Pairing(BluetoothDeviceInfo deviceInfo)
+        {
+            // Creates a paring request, and waits until a value is returned (signifying that the Task is complete)
+            bool paired = BluetoothSecurity.PairRequest(deviceInfo.DeviceAddress, null);
+
+            if (!paired)
+            {
+                return false;
+            }
+
+            deviceInfo = new BluetoothDeviceInfo(deviceInfo.DeviceAddress);
+
+            // List object to store the services offered by the paired device
+            List<Guid> serviceGuidList = new List<Guid>();
+            // Gets the services info of the pairing device using L2Cap
+            ServiceRecord[] serviceinfo = deviceInfo.GetServiceRecords(BluetoothService.L2CapProtocol);
+            foreach (var record in serviceinfo)
+            {
+                // ServiceRecordUtilities.Dump(Console.Error, record);
+                // Gets the ServiceDescription record from the detected services
+                ServiceAttribute sdpRecord = record.GetAttributeById(InTheHand.Net.Bluetooth.AttributeIds.UniversalAttributeId.ServiceDescription);
+                if (sdpRecord != null)
+                {
+                    serviceGuidList.Add(sdpRecord.Value.GetValueAsElementArray()[0].GetValueAsUuid());
+                }
+            }
+
+            return true;
+        }
+
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
